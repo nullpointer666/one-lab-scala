f9e0f6c2642ee01df455548618e65e18d190276f
Azamat Rsymbetov <seetwopluses@gmail.com>
2020-06-26 22:55:09 +0600

    Implemented tailRecursiveFibonacciNumber

--- src/main/scala/one/lab/tasks/week/one/Recursion.scala
+++ src/main/scala/one/lab/tasks/week/one/Recursion.scala
@@ -1,11 +1,20 @@
 package one.lab.tasks.week.one
 
+import scala.annotation.tailrec
+
 object Recursion {
   def printNTimes(n: Int, value: String): Unit = ???
 
   def gcd(a: Long, b: Long): Long = ???
 
-  def nthFibonacciNumber(n: Int): Int = if (n <= 0) 0 else if (n <= 2) 1 else nthFibonacciNumber(n - 1) + nthFibonacciNumber(n - 2)
+  def nthFibonacciNumber(n: Int): Int =
+    if (n <= 0) 0 else if (n <= 2) 1 else nthFibonacciNumber(n - 1) + nthFibonacciNumber(n - 2)
 
-  def tailRecursiveFibonacciNumber(n: Int): Int = ???
+  def tailRecursiveFibonacciNumber(n: Int): Int = {
+    @tailrec
+    def fibonacciUtil(accumulator: Int, previousValue: Int, counter: Int): Int =
+      if (counter >= n) accumulator else fibonacciUtil(accumulator + previousValue, accumulator, counter + 1)
+
+    if (n <= 0) 0 else fibonacciUtil(1, 1, 2)
+  }
 }
