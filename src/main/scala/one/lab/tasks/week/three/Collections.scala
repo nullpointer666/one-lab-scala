61d1ca491a75ee1c261dbbd91e9f35151a6a15e5
Azamat Rsymbetov <seetwopluses@gmail.com>
2020-07-12 17:23:46 +0600

    Implemented Collections.duplicateNTimes

--- src/main/scala/one/lab/tasks/week/three/Collections.scala
+++ src/main/scala/one/lab/tasks/week/three/Collections.scala
@@ -1,10 +1,25 @@
 package one.lab.tasks.week.three
 
+import scala.annotation.tailrec
+
 object Collections {
 
   // duplicateNTimes(3, List(1,2,3)) == List(1,1,1,2,2,2,3,3,3)
   // duplicateNTimes(3, List()) == List()
-  def duplicateNTimes[A](n: Int, list: List[A]): List[A] = ???
+  def duplicateNTimes[A](n: Int, list: List[A]): List[A] = {
+    @tailrec
+    def duplicateNTimesUtil(elem: A, cnt: Int = n, accumulator: List[A] = List[A]()): List[A] =
+      if (cnt > 0) duplicateNTimesUtil(elem, cnt - 1, accumulator ++ List(elem))
+      else accumulator
+
+    @tailrec
+    def duplicateEveryElement(accumulator: List[A] = List(), temp: List[A] = list): List[A] = temp match {
+      case Nil => accumulator
+      case elem :: rest => duplicateEveryElement(accumulator ++ duplicateNTimesUtil(elem), rest)
+    }
+
+    duplicateEveryElement()
+  }
 
   // splitAtK(4, List(1,2,3,4,5,6,7,8,9)) == (List(1,2,3,4), List(5,6,7,8,9))
   // splitAtK(0, List(1,2,3)) == (List(), List(1,2,3))
